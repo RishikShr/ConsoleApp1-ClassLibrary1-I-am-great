8b2e162a58a9e1612fafe58a6b1b8a9cbd7a4dae
RISHIK SHRIVASTAV <rishikshrivastav0@gmail.com>
2023-01-10 18:58:41 +0530

    Add project files.

---
+++ ConsoleApp1-ClassLibrary1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassLibrary1;
+
+namespace ConsoleApp1ClassLibrary
+{   
+    class Program {
+        static void Main(string[] args)
+        {
+            ClassLibrary ob = new ClassLibrary();
+            ob.message();
+            Console.Read();
+        }   
+    }
+
+    
+}
+/*Console.WriteLine("Hello, World!");*/
